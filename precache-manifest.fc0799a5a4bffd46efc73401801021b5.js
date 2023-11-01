ba0c77385614c8d6b4a1522d9127a3419866228a
Sifan Jhetam <sifanjhetam1@gmail.com>
2023-11-01 12:33:00 +0530

    Updates

---
+++ precache-manifest.fc0799a5a4bffd46efc73401801021b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7887cbb3641f62fe281db2c675de9cef",
+    "url": "/sifan-Jhetam/task-master/index.html"
+  },
+  {
+    "revision": "006047642c18c50cdcd3",
+    "url": "/sifan-Jhetam/task-master/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "3798cb6426f0b94585bd",
+    "url": "/sifan-Jhetam/task-master/static/css/main.15b7b633.chunk.css"
+  },
+  {
+    "revision": "006047642c18c50cdcd3",
+    "url": "/sifan-Jhetam/task-master/static/js/2.bcd36ba8.chunk.js"
+  },
+  {
+    "revision": "cda3a4660b5beb33de5753aac1ba91cb",
+    "url": "/sifan-Jhetam/task-master/static/js/2.bcd36ba8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3798cb6426f0b94585bd",
+    "url": "/sifan-Jhetam/task-master/static/js/main.fa188a73.chunk.js"
+  },
+  {
+    "revision": "a41eb6d006a29567d051",
+    "url": "/sifan-Jhetam/task-master/static/js/runtime-main.f6758027.js"
+  }
+]);
